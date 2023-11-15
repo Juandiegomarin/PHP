db93234847ac1b81c9d4eb843ffe2a5fd1d4df13
Juandiegomarin <jmarmor2111@g.educaand.es>
2023-11-15 14:41:23 +0100

    listado

--- Pract_8/index.php
+++ Pract_8/index.php
@@ -1,3 +1,36 @@
+<?php
+echo"<h1>Practica 8</h1>";
+if(isset($_POST["botonDetalle"])){
+
+try {
+    //Realizar conexión
+    $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_cv");
+    mysqli_set_charset($conexion, "utf8");
+} catch (Exception $e) {
+    die("<p>No ha podido conectarse a la a base de datos:" . $e->getMessage() . "</p></body></html>");
+}
+
+
+
+try {
+    $consulta = "select * from usuarios where id_usuario='".$_POST["botonDetalle"]."'";
+    $resultado = mysqli_query($conexion, $consulta);
+    mysqli_close($conexion);
+} catch (Exception $e) {
+    mysqli_close($conexion);
+    die("<p>No ha podido realizar la consulta:" . $e->getMessage() . "</p></body></html>");
+}
+
+    $detalle_usuario=mysqli_fetch_assoc($resultado);
+    echo "<h2>Detalles del usuario:</h2>";
+    echo "<p><span class='negrita'>Usuario: </span>".$detalle_usuario["usuario"]."</p>";
+    echo "<p><span class='negrita'>Nombre: </span>".$detalle_usuario["nombre"]."</p>";
+    echo "<p><span class='negrita'>Dni: </span>".$detalle_usuario["dni"]."</p>";
+    echo "<p><span class='negrita'>Sexo: </span>".$detalle_usuario["sexo"]."</p>";
+    echo "<p><span class='negrita'>Foto: </span><br><img src=img/".$detalle_usuario["foto"]." width='100px'>'</p>";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +39,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica 8</title>
     <style>
+        .negrita{
+            font-weight: bold;
+        }
         table,
         td,
         th {
             border: 1px solid black;
         }
+        th,td{
+            width: 10rem;        }
 
         table {
             border-collapse: collapse;
@@ -32,9 +70,6 @@
 
 <body>
 
-    <h1>Practica 8</h1>
-
-
 
     <?php
     try {
@@ -50,11 +85,12 @@
     try {
         $consulta = "select * from usuarios";
         $resultado = mysqli_query($conexion, $consulta);
+        mysqli_close($conexion);
     } catch (Exception $e) {
         mysqli_close($conexion);
         die("<p>No ha podido realizar la consulta:" . $e->getMessage() . "</p></body></html>");
     }
-
+    
     
     
     echo " <h2>Listado de usuarios</h2> ";
@@ -73,7 +109,7 @@
         else
         echo "<td>".$arr["foto"]."</td>";
         
-        echo "<td><form action='index.php' method='post'><button class='enlace' type='submit' name='botonDetalle'>".$arr['nombre']."</button></form></td>";
+        echo "<td><form action='index.php' method='post'><button class='enlace' type='submit' name='botonDetalle' value='".$arr["id_usuario"]."'>".$arr['nombre']."</button></form></td>";
         echo "<td><form action='index.php' method='post'><button class='enlace' type='submit' name='botonBorrar'>Borrar</button><span class='azul'>-</span><button class='enlace' type='submit' name='botonEditar'>Editar</button></form></td>";
         echo "</tr>";
     }
