2e1204b8235bdf2a33992813c7cfd2a2cbc7551a
Juandiegomarin <jmarmor2111@g.educaand.es>
2023-11-30 12:04:30 +0100

    EXAMEN

--- Examen2/index.php
+++ Examen2/index.php
@@ -20,6 +20,15 @@
     <body>
         <h1>Examen2 PHP</h1>
         <h2>Horario de los Profesores</h2>
-    
+
+        <?php
+            try {
+                $conexion=mysqli_connect("localhost","jose","josefa","bd_horarios_exam");
+                mysqli_set_charset($conexion,"utf8");
+            } catch (Exception $e) {
+                die("<p>No se ha podido conectar a la base de datos:".$e->getMessage()."</p></body></html>");
+            }
+        
+        ?>
     </body>
 </html>
